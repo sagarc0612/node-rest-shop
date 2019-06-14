f4bae9e94d257de82be9a57b9b3b1a98174cd51f
sagar.chavada <sagar.chavada@IFOUR-37>
2019-06-14 10:55:42 +0530

    Update server.js
    Add port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
